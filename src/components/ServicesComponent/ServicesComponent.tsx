276e4ba919593f614bc93fc4c864a3167d1ad9bc
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2024-10-17 20:09:15 +0530

    Core:Service component added

--- src/components/ServicesComponent/ServicesComponent.tsx
+++ src/components/ServicesComponent/ServicesComponent.tsx
@@ -1,8 +1,21 @@
 "use client";
 import React from "react";
+import dynamic from "next/dynamic";
+import { servicesData } from "@/testdata/service-data";
+const ServicesItem = dynamic(() => import("@/components/ServicesComponent/ServicesItem"));
 
 const ServicesComponent = () => {
-  return <div>ServicesComponent</div>;
+  return (
+    <div className="px-15">
+      <div className="heading flex justify-center items-center mt-14 mb-6 flex-col">
+        <div>Navigate seamlessly with our</div>
+        <div className="text-center">exclusive services</div>
+
+      </div>
+
+      <ServicesItem />
+    </div>
+  );
 };
 
 export default ServicesComponent;
--- src/components/ServicesComponent/ServicesItem.tsx
+++ src/components/ServicesComponent/ServicesItem.tsx
@@ -1,10 +1,35 @@
 "use client";
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { GoArrowRight } from "react-icons/go";
 
 const ServicesItem = () => {
   return (
-    <div>ServicesItem</div>
-  )
-}
+    <div className=' flex gap-[24px]'>
+      
+      <div className='h-[180px] w-[166px] border border-basic rounded-[12px] bg-secondary flex flex-col justify-between'>
+        <div className='ml-3 mt-3'>
+          <Image src="/webicon.png" alt="Services" width={48} height={48} />
+        </div>
+        <div className='flex justify-between items-center px-3 pb-3'>
+          <p className="text-[16px] max-w-[89px] font-medium leading-[17.92px]">Engineering Service</p>
+          <GoArrowRight className="w-[24px] h-[24px]" />
+        </div>
+      </div>
 
-export default ServicesItem
+      <div className='h-[180px] w-[166px] border border-basic rounded-[12px] bg-secondary flex flex-col justify-between'>
+        <div className='ml-3 mt-3'>
+          <Image src="/webicon2.png" alt="Services" width={48} height={48} />
+        </div>
+        <div className='flex justify-between items-center px-3 pb-3'>
+          <p className="text-[16px] max-w-[89px] font-medium leading-[17.92px]">Rentel  Service</p>
+          <GoArrowRight className="w-[24px] h-[24px]" />
+        </div>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default ServicesItem;
