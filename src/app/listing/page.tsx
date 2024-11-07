36c52874c66b046e89cdfeaeae2c02844de9bfbf
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2024-11-07 08:37:45 +0530

    core:filter

--- src/app/listing/page.tsx
+++ src/app/listing/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import dynamic from 'next/dynamic'
+const Filter = dynamic(() => import('@/app/listing/filter'))
 
 
 const ListingPage = () => {
   return (
-    <div>ListingPage</div>
+    <div className=''>
+      <div className="bg-secondary h-[56px] flex items-center w-[320px] p-3 border-b border-basic">
+        <p className="text-[16px] leading-[17px] font-semibold">Filters</p>
+      </div>
+      <Filter />
+    </div>
   )
 }
+
 
 export default ListingPage
