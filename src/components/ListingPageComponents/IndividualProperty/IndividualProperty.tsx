5f702dc259163dc4d0bbcd7602cfba423f505ac2
mahi25soni <msdraj02@protonmail.com>
2024-11-08 18:24:27 +0530

    Core : Second element of individual property added

--- src/components/ListingPageComponents/IndividualProperty/IndividualProperty.tsx
+++ src/components/ListingPageComponents/IndividualProperty/IndividualProperty.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 
 interface SinglePropertyInterface {
@@ -13,6 +13,17 @@ interface SinglePropertyInterface {
 
 const IndividualProperty = (data: SinglePropertyInterface) => {
     const [selectedImage, setSelectedImage] = useState<string>("")
+    const scrollRef = useRef<HTMLDivElement>(null)
+
+    const handleScrollRight = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: 600,
+                behavior: "smooth",
+            });
+        }
+    };
+
     return (
         <div className="absolute right-0 top-0 z-1000">
 
@@ -34,7 +45,7 @@ const IndividualProperty = (data: SinglePropertyInterface) => {
                 </div>
 
                 <div className='relative w-full'>
-                    <div className='flex gap-[2px] overflow-x-auto w-full'>
+                    <div className='flex gap-[2px] overflow-hidden w-full' ref={scrollRef}>
                         {
                             data?.images?.map((image, index) => {
                                 return <div className={`w-[68.25px] h-[44px] cursor-pointer p-1 border-2 rounded-md ${selectedImage === image?.id ? "border-cta-border" : "border-transparent"}`} onClick={() => setSelectedImage(image.id)} key={image?.id}>
@@ -53,9 +64,14 @@ const IndividualProperty = (data: SinglePropertyInterface) => {
                         }
 
                     </div>
-                    <div>
-
+                    <div className='absolute right-0 w-10 h-10 rounded-70 border border-basic flex items-center justify-center top-0 bg-white' onClick={handleScrollRight}>
+                        <Image src="/Arrow-right.svg" height={24} width={24} alt='Arrow-right' className='text-cta-darker' />
                     </div>
+                </div>
+
+
+                <div>
+
                 </div>
             </div>
         </div>
