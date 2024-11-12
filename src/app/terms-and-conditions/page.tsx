3f4c12048ddca4882e437343e56e231dbbf047b6
mahi25soni <msdraj02@protonmail.com>
2024-11-12 18:22:51 +0530

    Core : tnc page started

---
+++ src/app/terms-and-conditions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TermsAndCondition = (props: Props) => {
+    return (
+        <div>TermsAndCondition</div>
+    )
+}
+
+export default TermsAndCondition
