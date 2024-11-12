ad34c626b349dcbf1562ae940dd7113e7a9c301d
mahi25soni <msdraj02@protonmail.com>
2024-11-12 19:56:51 +0530

    Core : Testimonial mobiel view

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -25,8 +25,9 @@ const SingleCommment = ({
   profile_pic,
 }: TestimonialData) => {
   return (
-    <div className="border-1 flex h-[316px] min-w-[844px] flex-col items-start justify-between gap-6 rounded-20 border-basic bg-white p-5">
-      <div className="overflow-y-auto leading-[22px] text-light-gray">
+    <div className="flex lg:h-[316px] h-[450px] lg:min-w-[844px] min-w-full flex-col items-start justify-between gap-6 rounded-20 border-basic
+     border bg-white p-5">
+      <div className="overflow-y-auto leading-[22px] text-light-gray scrollbar-hidden">
         {comment}
       </div>
       <div className="h-min-15 flex gap-5">
@@ -56,26 +57,33 @@ const Testimonials = () => {
 
   const handleScrollRight = () => {
     if (trackScrollRef.current) {
+      const child = trackScrollRef.current.firstElementChild as HTMLElement | null;
+      const childWidth = child?.offsetWidth || 0;
       trackScrollRef.current.scrollBy({
-        left: 1000,
-        behavior: "smooth",
-      });
-    }
-  };
-  const handleScrollLeft = () => {
-    if (trackScrollRef.current) {
-      trackScrollRef.current.scrollBy({
-        left: -1000,
+        left: childWidth,
         behavior: "smooth",
       });
     }
   };
 
+  const handleScrollLeft = () => {
+    if (trackScrollRef.current) {
+      const child = trackScrollRef.current.firstElementChild as HTMLElement | null;
+      const childWidth = child?.offsetWidth || 0;
+      trackScrollRef.current.scrollBy({
+        left: -childWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
+
+
   return (
-    <div className="mt-20 px-5 lg:px-15">
+    <div className="mt-20 lg:pl-15 px-5 lg:pr-0">
       <SingleHeading firstLine="What people say about us" />
       <div
-        className="mt-10 flex w-full items-center justify-start gap-4 overflow-hidden"
+        className="mt-10 flex w-full items-center justify-start  overflow-hidden lg:gap-4 sm:gap-3"
         ref={trackScrollRef}
       >
         {testimonialData.map((data) => (
