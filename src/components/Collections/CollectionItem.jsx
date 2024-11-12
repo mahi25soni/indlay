c379f7077b88a4762e39ae41ef1fee88c0cfe91d
mahi25soni <msdraj02@protonmail.com>
2024-11-12 15:50:09 +0530

    Core : Collections mobile view

--- src/components/Collections/CollectionItem.jsx
+++ src/components/Collections/CollectionItem.jsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 const CollectionItem = () => {
   return (
-    <div className="mt-6 flex gap-4 w-full"> 
+    <div className="mt-6 flex w-full gap-4">
       {/* Card 1 */}
-      <div className="flex-1 gap-2 "> 
-        <div className="grid w-full grid-cols-2 gap-2"> 
+      <div className="flex-1 gap-2">
+        <div className="grid w-full grid-cols-2 gap-2">
           <div className="relative col-span-2 min-h-[180px] w-full overflow-hidden rounded-[20px] bg-[#C6F8FF]">
             <div className="relative z-10 flex h-full items-center justify-between px-4">
               <p className="h-[54px] w-[103px] text-[24px] font-medium leading-[26.88px] text-[#00343C]">
@@ -17,9 +17,9 @@ const CollectionItem = () => {
                 <Image
                   src="/collection1.png"
                   alt="Logo"
-                  width={268} 
-                  height={333} 
-                  className="absolute bottom-0 right-0 h-auto" 
+                  width={268}
+                  height={333}
+                  className="absolute bottom-0 right-0 h-auto"
                 />
               </div>
             </div>
@@ -33,9 +33,9 @@ const CollectionItem = () => {
               <Image
                 src="/collection2.png"
                 alt="Logo"
-                width={322} 
+                width={322}
                 height={92}
-                className="mt-auto" 
+                className="mt-auto"
               />
             </div>
           </div>
@@ -48,8 +48,8 @@ const CollectionItem = () => {
               <Image
                 src="/collection3.png"
                 alt="Logo"
-                width={393} 
-                height={263} 
+                width={393}
+                height={263}
                 className="mt-auto"
               />
             </div>
@@ -58,7 +58,7 @@ const CollectionItem = () => {
       </div>
 
       {/* Card 2 */}
-      <div className="h-[470px] flex-1 rounded-[20px] bg-[#FFE4DE]"> 
+      <div className="h-[470px] flex-1 rounded-[20px] bg-[#FFE4DE]">
         <div className="flex h-full flex-col">
           <p className="mx-auto mt-6 h-[27px] w-[172px] text-[24px] font-medium leading-[26px] text-[#4B0F01]">
             Category Name
@@ -66,15 +66,15 @@ const CollectionItem = () => {
           <Image
             src="/collection4.png"
             alt="Logo"
-            width={568} 
-            height={380} 
-            className="mt-auto h-[60%] w-full rounded-b-[20px] object-cover" 
+            width={568}
+            height={380}
+            className="mt-auto h-[60%] w-full rounded-b-[20px] object-cover"
           />
         </div>
       </div>
 
       {/* Card 3 */}
-      <div className="flex flex-col flex-1"> 
+      <div className="flex flex-1 flex-col">
         <div className="relative flex min-h-[300px] w-full flex-col items-center justify-between rounded-[20px] bg-[#C8FFEE] p-4">
           <p className="mx-auto h-[54px] w-[103px] text-center text-[24px] font-medium leading-[26.88px] text-[#44004F]">
             Category Name
@@ -82,18 +82,18 @@ const CollectionItem = () => {
           <Image
             src="/collection5.png"
             alt="Logo"
-            width={586} 
-            height={389} 
-            className="absolute bottom-0 h-auto w-full rounded-[20px] object-contain" 
+            width={586}
+            height={389}
+            className="absolute bottom-0 h-auto w-full rounded-[20px] object-contain"
           />
         </div>
 
-        <div className="mt-4 h-[156px] w-full rounded-[20px] bg-[#C8F2FF]"> 
+        <div className="mt-4 h-[156px] w-full rounded-[20px] bg-[#C8F2FF]">
           <div className="relative z-10 flex h-full items-center justify-between px-4">
             <p className="h-[54px] w-[103px] text-[24px] font-medium leading-[26.88px] text-[#00343C]">
               Category Name
             </p>
-            <div className="h-full flex-shrink-0">
+            {/* <div className="h-full flex-shrink-0">
               <Image
                 src="/collection6.png"
                 alt="Logo"
@@ -101,7 +101,7 @@ const CollectionItem = () => {
                 height={210} 
                 className="h-full w-auto object-contain" 
               />
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
--- src/components/Collections/Collections.jsx
+++ src/components/Collections/Collections.jsx
@@ -20,6 +20,13 @@ const Collections = () => {
       </HeadingAndButton>
 
       <CollectionItem />
+
+      <div className="mt-10 flex justify-center">
+        <button className="flex items-center justify-center gap-3 rounded-70 border border-basic bg-secondary px-3 py-2 text-base font-medium text-cta-darker">
+          <p className="text-sm">View all</p>
+          <GoArrowRight className="h-5 w-5" />
+        </button>
+      </div>
     </div>
   );
 };
