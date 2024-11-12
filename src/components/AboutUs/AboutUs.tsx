325a9e7c3498edd1ac5e0a500fa50caca07314ee
mahi25soni <msdraj02@protonmail.com>
2024-11-12 18:44:11 +0530

    Core : about component mobile view

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import { aboutSampleData } from "@/testdata/about-data";
-import { motion } from "framer-motion"; 
+import { motion } from "framer-motion";
 import { FaRegDotCircle } from "react-icons/fa";
 
 const SingleHeading = dynamic(
@@ -67,40 +67,51 @@ const AboutUs = () => {
   }, [isIntersecting]);
 
   return (
-    <div className="mt-20 flex w-full flex-col gap-8 px-15" ref={sectionRef}>
-      <SingleHeading firstLine="Why people choose us?" />
-      <div className="flex items-center justify-center">
-        <p className="max-w-[600px] text-center text-base font-normal leading-4 text-light-gray">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+    <div className="mt-20 flex w-full flex-col gap-10 lg:gap-[60px] px-5 lg:px-15" ref={sectionRef}>
+      <div className="w-full flex gap-4 flex-col lg:gap-6">
+
+        <SingleHeading firstLine="Why people choose us?" />
+        <div className="flex items-center justify-center">
+          <p className="max-w-[600px] text-center text-base font-normal leading-[15.68px] text-light-gray">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="lg:grid lg:grid-cols-2 lg:gap-4 flex flex-col w-full gap-6">
         {/* Left section with headings and paragraphs */}
+        <div className="relative h-[370px] w-full lg:hidden">
+          <Image
+            src={data[currentIndex].icon}
+            alt={data[currentIndex].heading}
+            fill
+            // sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+            className="rounded-3xl object-fill"
+          />
+        </div>
         <div className="flex flex-col gap-6">
           {data.map((item, index) => (
-            <div key={index} className={`flex flex-col`}>
+            <div key={index} className={`flex-col text-center lg:text-left ${currentIndex !== index ? "hidden lg:flex" : "lg:flex"}`}>
               <div>
                 <div className="grid grid-cols-12">
-                  <div className="col-span-1 p-1">
+                  <div className="col-span-1 p-1 hidden lg:block">
                     {currentIndex === index ? (
                       <FaRegDotCircle className="h-6 w-6 text-cta-darker" />
                     ) : (
                       <div></div>
                     )}
                   </div>
-                  <div className="col-span-11">
-                    <div className="flex flex-col">
+                  <div className="col-span-12 lg:col-span-11">
+                    <div className="flex flex-col gap-4 lg:gap-0">
                       <p className={`} mb-2 text-2xl font-medium`}>
                         {item?.heading}
                       </p>
                       <p
-                        className={`mb-4 text-base font-normal ${
-                          currentIndex === index
-                            ? "text-black"
-                            : "text-light-gray"
-                        }`}
+                        className={`mb-4 text-base font-normal ${currentIndex === index
+                          ? "text-black"
+                          : "text-light-gray"
+                          }`}
                       >
                         {item?.note}
                       </p>
@@ -119,7 +130,8 @@ const AboutUs = () => {
             </div>
           ))}
         </div>
-        <div className="relative h-full w-full">
+
+        <div className="relative h-full w-full hidden lg:block">
           <Image
             src={data[currentIndex].icon}
             alt={data[currentIndex].heading}
@@ -128,6 +140,7 @@ const AboutUs = () => {
             className="rounded-3xl object-fill"
           />
         </div>
+
       </div>
     </div>
   );
