105e3cf23aa10a21f58fd19eb5bea30255382928
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2024-11-13 20:13:19 +0530

    core:page and about and contact :mobile view done

--- src/components/AboutPageComponents/AboutService/AboutService.tsx
+++ src/components/AboutPageComponents/AboutService/AboutService.tsx
@@ -19,7 +19,7 @@ const AboutService = () => {
   const [aboutDataList, setAboutDataList] = useState<AboutServiceData[]>([]);
 
   useEffect(() => {
-    setAboutDataList(aboutServiceData); 
+    setAboutDataList(aboutServiceData);
   }, []);
 
   return (
@@ -27,19 +27,33 @@ const AboutService = () => {
     <SingleHeading firstLine="Services we provide" secondLine="to our esteemed customers" />
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-10">
       {aboutDataList.map((service, index) => (
-        <div key={index} className="min-w-[312px] min-h-[300px] rounded-xl border-[1.5px] border-basic bg-secondary flex flex-col justify-between p-4">
-          <div className="flex justify-end">
-            <Image src={service.icon} alt={service.heading_name} width={60} height={60} />
+        <div 
+          key={index} 
+          className="relative min-w-[350px] lg:min-w-[312px] min-h-[180px] lg:min-h-[300px] rounded-xl border-[1.5px] border-basic bg-secondary flex flex-col p-4"
+        >
+          <div className="absolute top-4 right-4 h-[48px] w-[48px] lg:h-[60px] lg:w-[60px]">
+            <Image 
+              src={service.icon} 
+              alt={service.heading_name} 
+              fill 
+              className="object-cover" 
+            />
           </div>
+  
           <div className="flex flex-col gap-3 mt-auto">
-            <h2 className="text-2xl leading-[26px] font-medium">{service.heading_name}</h2>
-            <p className="h-[54px] leading-[17px] text-base font-normal text-light-gray">{service.paragraph}</p>
+            <h2 className="text-[16px] lg:text-[24px] leading-[17.92px] lg:leading-[26px] font-medium">
+              {service.heading_name}
+            </h2>
+            <p className="h-[54px] text-[14px] lg:text-[16px] leading-[17px] text-base font-normal text-light-gray">
+              {service.paragraph}
+            </p>
           </div>
         </div>
       ))}
     </div>
   </div>
   
+
   );
 }
 
--- src/components/ContactPageComponents/ContactOptions/ContactOptions.tsx
+++ src/components/ContactPageComponents/ContactOptions/ContactOptions.tsx
@@ -18,21 +18,26 @@ interface ContactOptionsProps {
 
 const ContactOptions: React.FC<ContactOptionsProps> = ({ contacts = contactData }) => {
   return (
-    <div className="w-full px-15 mt-8">
+    <div className="w-full px-5 lg:px-15 mt-8">
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
         {contacts.map((contact) => (
           <div
             key={contact.id}
-            className="min-w-[312px] h-[240px] rounded-xl border bg-secondary border-basic p-4 flex flex-col justify-between"
+            className="min-w-[350px] md:min-w-[312px] h-[104px] lg:h-[240px] rounded-xl border bg-secondary border-basic p-2 lg:p-4 flex flex-row-reverse md:flex-row-reverse lg:flex-col  justify-between"
           >
-            <Image src={contact.icon} alt={contact.title} width={48} height={48} />
+            <div className="relative w-[40px]  lg:w-[48px] h-[40px] lg:h-[48px]">
+              <Image src={contact.icon} alt={contact.title} layout="fill"
+                objectFit="fill"
+              />
+
+            </div>
 
             <div className="flex flex-col gap-3">
-              <h2 className="font-medium text-2xl leading-[26.88px]">{contact.title}</h2>
-              <p className="font-normal text-base leading-[17px] text-[#9EA4AF]">
+              <h2 className="font-medium text-[20px] lg:text-[24px] leading-[22.4px] lg:leading-[26.88px]">{contact.title}</h2>
+              <p className="font-normal text-[14px] lg:text-[16px] leading-[15.68px] lg:leading-[17.92px] text-[#9EA4AF]">
                 {contact.description}
               </p>
-              <p className="font-medium text-xl text-[#022D42]">{contact.contactInfo}</p>
+              <p className="font-medium text-[16px] lg:text-[20px] leading-[17.92px] lg:leading-[22.4px] text-[#022D42]">{contact.contactInfo}</p>
             </div>
           </div>
         ))}
